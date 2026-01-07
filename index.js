3f43a9f37a5161429740309bb798a7b2d8b5bfce
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-07 20:13:54 +0000

    TURN YOUR FACE TOWARDS THE SUN

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function taxable (state , taxExempt) {
+    if ((state == IA) && (taxExempt == false)) {
+        return 'is taxable'
+    }
+}
 
+function isVehicle (hasWheels , canFly , canSwim) {
+    if ((hasWheels == true) || (canFly == true) || canSwim == true) {
+        return 'is vehicle'
+    }
+}
+
+//should create a function named isVehicle that takes three parameters hasWheels, canFly, canSwim and returns the string "is vehicle" if at least one of the three parameters evaluates to true
+//should return the string "is vehicle" when hasWheels is true and canFly and canSwim are both false
+//should return the string "is vehicle" when canFly is true and hasWheels and canSwim are both false
+//should return the string "is vehicle" when canSwim is true and canFly and hasWheels are both false
+//should return the string "is vehicle" when canSwim and canFly are both true and hasWheels is false
+//should not return the string "is vehicle" when hasWheels, canFly, and canSwim are all false
